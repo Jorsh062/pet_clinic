47cf05ba51131df6bf2149495d8e7f8edb8ae6b9
Steve Jones <jones.iredbraincoffee@gmail.com>
2021-03-08 12:06:45 +0100

    added layout to index page

--- pet_clinic/pet-clinic-data/src/main/java/services/OwnerService.java
+++ pet_clinic/pet-clinic-data/src/main/java/services/OwnerService.java
@@ -5,6 +5,7 @@ import model.Owner;
 import java.util.List;
 import java.util.Set;
 
+
 public interface OwnerService extends CrudeService<Owner,Long> {
     Owner findByLastName(String lastName);
 
--- pet_clinic/pet-clinic-web/src/main/java/PetClinic/controllers/OwnerController.java
+++ pet_clinic/pet-clinic-web/src/main/java/PetClinic/controllers/OwnerController.java
@@ -1,6 +1,7 @@
 package PetClinic.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,6 @@ public class OwnerController {
 
 
     private final OwnerService ownerService;
-
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
