082d6987afa37fe18bdfe572cb1de34a7b591f64
Steve Jones <jones.iredbraincoffee@gmail.com>
2021-03-08 15:11:21 +0100

    added pet type service

---
+++ pet_clinic/pet-clinic-data/src/main/java/model/Specialty.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Set;
+
+public class Specialty extends BaseEntity{
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+}
